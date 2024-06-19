bb13dc31abc2305dea6db7ffb74b1f4a8d3095e6
Paul Lam <109431478+Pauullamm@users.noreply.github.com>
2024-06-19 12:09:28 +0000

    mock schema update

--- command.py
+++ command.py
@@ -2,22 +2,35 @@ import psycopg2
 from config import load_config
 
 def command():
-    commands = ("""
-                CREATE TABLE test (
+    commands = """
+                CREATE TABLE Profiles (
                 id SERIAL PRIMARY KEY,
-                name VARCHAR NOT NULL
+                first_name VARCHAR(255),
+                last_name VARCHAR(255),
+                email VARCHAR(255),
+                gender CHAR(255),
+                user_id VARCHAR(255),
+                username VARCHAR(255),
+                birthdate VARCHAR(255),
+                location VARCHAR(255),
+                bio TEXT,
+                profile_pic VARCHAR(255),
+                followers_count INT,
+                is_verified BOOL,
+                interests TEXT
                 );
-                """,
                 """
-                INSERT INTO test
-                VALUES(DEFAULT, 'Apple');
-                """)
     try:
         config = load_config()
         with psycopg2.connect(**config) as conn:
             with conn.cursor() as cur:
+                # Split the string into separate SQL commands
+                commands = commands.split(";")
+
+                # Execute each command
                 for command in commands:
-                    cur.execute(command)
+                    if command.strip():  # Skip empty lines
+                        cur.execute(command.strip())
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 

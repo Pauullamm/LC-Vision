a41d79d2ddf9f55ae3b4982857fc599c89a9cf80
Paul Lam <109431478+Pauullamm@users.noreply.github.com>
2024-06-26 11:12:47 +0000

    added streaming of response

--- lc.py
+++ lc.py
@@ -1,14 +1,18 @@
-from im_t import response
 from CustomDocLoader import CustomDocumentLoader
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores import LanceDB
 
 import os
 
 api_key = os.environ["OPENAI_API_KEY"]
 path_to_file = ""
-img_info = response.json()["choices"][0]["message"]["content"]
 loader = CustomDocumentLoader(path_to_file)
 docs = loader.load()
-llm = ChatOpenAI(openai_api_key=api_key, max_tokens=1000, model='gpt-4-turbo')
+llm = ChatOpenAI(openai_api_key=api_key, max_tokens=1000, model='gpt-4o')
 embeddings_model = OpenAIEmbeddings(openai_api_key=api_key, model='text-embedding-3-large', dimensions=1536)
+
+vector_store = LanceDB.from_documents(
+    documents=docs,
+    embedding=embeddings_model
+)
---
+++ stream.py
@@ -0,0 +1,39 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+from lc import llm, vector_store
+from im_t import response
+
+img_info = response.json()["choices"][0]["message"]["content"]
+
+prompt = ChatPromptTemplate.from_template("""
+Use the following context and any existing knowledge of medicines to answer the question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+If you don't know the answer, do not provide any information about manufacturers or drugs, but do explain why you don't know the answer.
+Use three sentences maximum and keep the answer as concise as possible.
+
+{context}
+
+Question: {input}""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+retriever = vector_store.as_retriever()
+def format_docs(docs):
+    return "\n\n".join([d.page_content for d in docs])
+
+
+retrieval_chain = (
+    {"context": retriever | format_docs, "input": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+question_to_ask = f"{img_info} - Based on the description mentioned, what drug does it describe and what is its manufacturer?"
+output_message = retrieval_chain.invoke(question_to_ask)
+for i in output_message.split("."):
+  print(i)

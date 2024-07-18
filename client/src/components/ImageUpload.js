36e73dfcf7649bc8a3544f7cbb514758082dbc38
Paul Lam <109431478+Pauullamm@users.noreply.github.com>
2024-07-18 14:00:41 +0000

    Styling changes: updated background colour of body centered input element

--- client/src/components/ImageUpload.js
+++ client/src/components/ImageUpload.js
@@ -1,11 +1,13 @@
 // ImageUpload.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import Loader from './Loader';
 
 function ImageUpload() {
   const [selectedFiles, setSelectedFiles] = useState(null);
   const [resizedImage, setResizedImage] = useState(null);
   const [outputResponse, setOutputResponse] = useState("");
+  const [uploadImage, setUploadImage] = useState(false)
 
   const handleFileChange = (event) => {
     setSelectedFiles(event.target.files);
@@ -41,19 +43,26 @@ function ImageUpload() {
 
   const handleUpload = async () => {
     const formData = new FormData();
+    if (!selectedFiles) {
+      console.log("Please choose an image first");
+      return
+    };
+
     for (let i = 0; i < selectedFiles.length; i++) {
       formData.append('images', selectedFiles[i]);
     }
-
+    setUploadImage(true)
     try {
-      const port = process.env.BE_EP
+      const port = "https://lc-vision.onrender.com/upload"//process.env.BE_EP
       const response = await axios.post(port, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
       const ai_res = response.data.message.choices[0].message.content;
+      setUploadImage(false);
       setOutputResponse(ai_res);
+      
     } catch (error) {
       console.error('Error uploading images', error);
     }
@@ -61,18 +70,24 @@ function ImageUpload() {
 
   return (
     <div class="flex flex-col">
+      <h1 class="mt-5 text-gray-300 text-2xl font-bold">Select an image to upload</h1>
+
       <div class="flex justify-center">
-        <div class="flex justify-center py-2 px-2 border-solid border-2 w-max h-max rounded-md my-10">
-          <input type="file" multiple onChange={handleFileChange} />
-          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" onClick={handleUpload}>Upload</button>
+        <div class="flex justify-center py-2 px-2 border-solid border-2 w-max h-max rounded-md mt-5">
+          <div class="content-center">
+            <input class="text-gray-300" type="file" multiple onChange={handleFileChange} />
+          </div>
+          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center cursor-pointer" onClick={handleUpload}>Upload</button>
         </div>
+        
       </div>
-      
       <div class="flex justify-center">
+        
         {resizedImage && <img src={resizedImage} alt="Selected" style={{ marginTop: '20px', maxWidth: '100%' }} />}
       </div>
+      {uploadImage && <Loader />}
+      |<div class="flex justify-center"><p class="w-1/2 text-gray-300">{outputResponse}</p></div>
       
-      <p>{outputResponse}</p>
     </div>
   );
 }
---
+++ client/src/components/Loader.js
@@ -0,0 +1,12 @@
+
+export default function Loader() {
+
+    return (
+        <div class="flex justify-center">
+            <svg viewBox="25 25 50 50" class="svg-spinner">
+                <circle r="10" cy="50" cx="50" class="circle-spinner"></circle>
+            </svg>
+        </div>
+            
+    )
+};

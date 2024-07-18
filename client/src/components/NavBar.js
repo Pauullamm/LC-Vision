fb81d61ff8779f165ffffea9a3e29d04d797618e
Paul Lam <109431478+Pauullamm@users.noreply.github.com>
2024-07-18 15:07:08 +0000

    Modify NavBar to receive user openai api key to send to backend

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 function Navbar() {
     const [apiSectionOpen, setApiSectionOpen] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+
     const toggleApiInput = () => {
         if (!apiSectionOpen) {
             setApiSectionOpen(true);
@@ -11,19 +13,59 @@ function Navbar() {
         }
     };
 
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleKeyDown = async (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+
+            const data = {input: inputValue};
+            const port = process.env.REACT_APP_BE_EP
+            try {
+                const response = await fetch(port, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                });
+                if (response.ok) {
+                    console.log(response);
+                } else {
+                    console.error('Error submitting data');
+                };
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+    }
+
     return (
             <nav class="bg-gray-800">
                 <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                     <div class="relative flex h-16 items-center justify-between">
                     <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 
-                        <div class="hidden sm:ml-6 sm:block">
+                        <div class="hidden sm:ml-5 sm:block">
                         <div class="flex space-x-3">
                             <a href="/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Dashboard</a>
                             <a href="https://www.medicines.org.uk/emc/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">EMC</a>
                             <button class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" onClick={toggleApiInput}>API Key</button>
                             <div className={`inputSection ${apiSectionOpen ? "open": ""} content-center`}>
-                            <input class="content-center appearance-none py-1 px-2 border border-black-500 rounded w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter API Key" />
+                            <input class="content-center 
+                            appearance-none 
+                            py-1 px-2 
+                            border border-black-500 
+                            rounded w-full text-gray-700 leading-tight 
+                            focus:outline-none focus:shadow-outline" 
+                            id="password" 
+                            type="password" 
+                            placeholder="Enter API Key" 
+                            onChange={handleInputChange}
+                            onKeyDown={handleKeyDown}
+                            />
                             </div>
                         </div>
                         </div>
